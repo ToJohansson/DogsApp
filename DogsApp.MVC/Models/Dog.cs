40889d86abdfe2197fe76bcf477c6a9b4076e8af
Tobias Johansson <tobias@noq.nu>
2025-05-14 13:01:58 +0200

    init model service

---
+++ DogsApp.MVC/Models/Dog.cs
@@ -0,0 +1,8 @@
+﻿namespace DogsApp.MVC.Models;
+
+public class Dog
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
---
+++ DogsApp.MVC/Services/DogService.cs
@@ -0,0 +1,22 @@
+﻿using DogsApp.MVC.Models;
+
+namespace DogsApp.MVC.Services;
+
+public class DogService
+{
+    private List<Dog> dogs = new List<Dog>();
+
+    public void AddDog(Dog dog)
+    {
+        dogs.Add(dog);
+    }
+    public Dog[] GetAllDogs()
+    {
+
+        return dogs.ToArray();
+    }
+    public Dog GetDogById(int id)
+    {
+        return dogs.Single(d => d.Id == id);
+    }
+}
